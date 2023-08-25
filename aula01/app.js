4ac635c1612f81fb95bab153f2e703a97e0eee04
ricksonl.berigo <ricksonl.berigo@outlook.com>
2023-08-25 16:03:53 -0300

    Finalizando aula 01

---
+++ aula01/app.js
@@ -0,0 +1,34 @@
+// for loop
+
+/*
+for (let i = 0; i < 5; i++) {
+  console.log(`Dentro do loop: ${i}`)
+}
+console.log('Loop concluído')
+*/
+
+/*
+const names = ['Rickson', 'Lucas', 'Silva', 'Ada', 'Bill', 'Kamilly']
+for (let i = 0; i < names.length; i++) {
+  const HTMLTemplate = `<p>${names[i]}</p>`
+  console.log(HTMLTemplate)
+}
+*/
+
+// While loop
+/*
+let i = 0
+
+while (i < 5) {
+  console.log(i)
+  i++
+}
+*/
+
+const names = ['Jobs', 'Hopper', 'Turing']
+let i = 0
+
+while (i < names.length) {
+  console.log(names[i])
+  i++
+}
