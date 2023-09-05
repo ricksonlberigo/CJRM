23bee6a5e322c1d2f78a885987c1739ada3d9a61
ricksonlberigo <ricksonl.berigo@outlook.com>
2023-09-04 21:00:09 -0300

    Iniciando o QUIZ

---
+++ app.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('.quiz-form')
+
+const correctAnswer = ['B', 'B', 'B', 'B']
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  let score = 0
+  constUserAnswer = [
+    
+  ]
+})
