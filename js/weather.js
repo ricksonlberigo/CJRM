19d0d957b9350e8e7126729a7f9c2852f5ebda3f
ricksonlberigo <ricksonl.berigo@outlook.com>
2023-09-20 17:03:22 -0300

    Iniciando a resolução dos exercícios

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,34 @@
 const APIKey = '735SJLAiJeJLNAhK9l20E1hG1aDujfnd'
+const getCityURL = cityName => `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
 
-// http://127.0.0.1:5500/index.html?city=Miaim&country=USA
+const getCityData = async cityName => {
+    try {
+        const cityUrl = getCityURL(cityName)
+        const response = await fetch(cityUrl)
+
+        if(!response.ok) throw new Error('Não foi possível obter os dados.')
+
+        const [cityData] = await response.json()
+        return cityData
+    } catch({name, message}) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+const getCityWeather = async cityName => {
+    try {
+        const {Key} = await getCityData(cityName)
+        const cityWeatherURL = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}`
+        const response = await fetch(cityWeatherURL)
+
+        if(!response.ok) throw new Error('Não foi possível obter os dados.')
+
+        const [cityWeatherData] = await response.json()
+        debugger
+        return cityWeatherData
+    } catch({name, message}) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+getCityWeather('São Paulo')
