11ff9ef5e36e0648c3e9f9fa3522b0cac78a482e
ricksonl.berigo <ricksonl.berigo@outlook.com>
2023-10-05 17:42:03 -0300

    Entendendo sobre constructor functions

--- aula03/app.js
+++ aula03/app.js
@@ -1,4 +1,29 @@
 // ==================================== Funções construtoras - Aula 03-06
+// class Student {
+//     constructor(name, email) {
+//         this.name = name
+//         this.email = email
+//     }
+// }
+
+function Student (name, email) {
+    this.name  = name
+    this.email = email
+
+    this.login = function () {
+        return `${this.name} fez login.`
+    }
+    this.logout = function () {
+        return `${this.name} efetuou logout.`
+    }
+}
+
+const gabrielFialho = new Student('Gabriel Fialho', 'gabrielf@rogermelo.com.br')
+const brenoLemos = new Student('Breno Lemos', 'brenolemos@rogermelo.com.br')
+
+console.log(gabrielFialho.login(), brenoLemos.login())
+console.log(gabrielFialho.logout(), brenoLemos.logout())
+
 // =====================================================================================
 
 // ==================================== Prototypes - Aula 03-07
